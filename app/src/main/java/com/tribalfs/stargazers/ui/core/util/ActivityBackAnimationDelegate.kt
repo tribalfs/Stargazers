749ba0e2212d02f7590ff2451da1c328a2cde794
tribalfs <tribalfs@gmail.com>
2024-11-21 17:00:32 +0800

    I feel this is a better interpolator

--- app/src/main/java/com/tribalfs/stargazers/ui/core/util/ActivityBackAnimationDelegate.kt
+++ app/src/main/java/com/tribalfs/stargazers/ui/core/util/ActivityBackAnimationDelegate.kt
@@ -1,19 +1,23 @@
 package com.tribalfs.stargazers.ui.core.util
 
+import android.animation.TimeInterpolator
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.graphics.Outline
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewOutlineProvider
+import android.view.animation.Interpolator
 import androidx.activity.BackEventCompat
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.animation.PathInterpolatorCompat
+import com.google.android.material.R
+import com.google.android.material.motion.MotionUtils
 import dev.oneuiproject.oneui.ktx.dpToPx
 import dev.oneuiproject.oneui.ktx.invokeOnBack
 import androidx.appcompat.R as appcompatR
 
-//TODO (consider moving to  design lib?)
+//TODO (move this to  design lib?)
 @SuppressLint("RestrictedApi")
 class ActivityBackAnimationDelegate private constructor() {
     private lateinit var activity: AppCompatActivity
@@ -23,8 +27,10 @@ class ActivityBackAnimationDelegate private constructor() {
     private var predictiveBackMargin : Int = 0
     private var initialTouchY = -1f
 
+    private lateinit var gestureInterpolator: TimeInterpolator
+
     companion object{
-        private val GestureInterpolator = PathInterpolatorCompat.create(0f, 0f, 0f, 1f)
+
         fun init(
             activity: AppCompatActivity,
             rootView: ViewGroup
@@ -33,7 +39,11 @@ class ActivityBackAnimationDelegate private constructor() {
                 this.activity = activity
                 this.rootView = rootView
                 activity.setWindowTransparent(true)
+                gestureInterpolator = MotionUtils.resolveThemeInterpolator(activity,
+                    R.attr.motionEasingStandardDecelerateInterpolator,
+                    PathInterpolatorCompat.create(0f, 0f, 0f, 1f))
                 predictiveBackMargin = 8.dpToPx(activity.resources)
+
                 setupPredictiveBackAnimation()
                 animateCornerRadiusToZero()
             }
@@ -71,12 +81,12 @@ class ActivityBackAnimationDelegate private constructor() {
             onBackProgressed = {backEvent ->
                 if (initialTouchY < 0f) initialTouchY = backEvent.touchY
 
-                val progress = GestureInterpolator.getInterpolation(backEvent.progress)
+                val progress = gestureInterpolator.getInterpolation(backEvent.progress)
                 val scale = 1f - (0.1f * progress)
                 val maxTranslationX = ((rootView.width / 20) - predictiveBackMargin) *
                         (if (backEvent.swipeEdge == BackEventCompat.EDGE_LEFT) 1 else -1)
 
-                val progressY = GestureInterpolator.getInterpolation(
+                val progressY = gestureInterpolator.getInterpolation(
                     (backEvent.touchY - initialTouchY) / rootView.height)
                 val maxTranslationY = (rootView.height / 20) - predictiveBackMargin
 
