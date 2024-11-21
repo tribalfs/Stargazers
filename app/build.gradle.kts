4cd497e3952d60ec80a99317afd707cc0512a55d
tribalfs <tribalfs@gmail.com>
2024-11-22 07:17:36 +0800

    Your promotion might be too quick, but here we are!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         minSdk = 23
         targetSdk = 35
 
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         vectorDrawables.useSupportLibrary = true
     }
