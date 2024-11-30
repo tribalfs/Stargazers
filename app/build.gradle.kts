e92a0d769bd063966ff85b4b3f4f1234ae2d97ef
tribalfs <tribalfs@gmail.com>
2024-12-01 02:49:25 +0800

    One step closer to 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         minSdk = 23
         targetSdk = 35
 
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         vectorDrawables.useSupportLibrary = true
     }
